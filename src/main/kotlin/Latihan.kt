cdc8249ab3a6da39fa5fdf22037184c5f1b607a4
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:19:14 +0700

    latihan data 1a

--- src/main/kotlin/Latihan.kt
+++ src/main/kotlin/Latihan.kt
@@ -1,10 +1,24 @@
 fun main () {
-    println("Hello World")
-    println("Diego")
+//    println("Hello World")
+//    println("Diego")
+//
+//    print("Pekerjaan : ")
+//    var pekerjaan = readLine()
+//    print("Alamat : ")
+//    var alamat = readLine()
+//    println("Pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
 
-    print("Pekerjaan : ")
-    var pekerjaan = readLine()
-    print("Alamat : ")
-    var alamat = readLine()
-    println("Pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+    //petemuan 2
+
+    var namaLengkap: String = "Diego"
+
+
+    var alamat: String
+    alamat = "Ruteng"
+    var umur =19
+    println (namaLengkap)
+    println (alamat)
+    println (umur)
+
 }
+
